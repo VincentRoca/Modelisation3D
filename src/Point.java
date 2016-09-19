82ccb1e8364493680fbc2afcf7db5a6d720434a2
adrien Lecomte <adrien.lecomte@etudiant.univ-lille1.fr>
2016-09-19 13:59:24 +0200

    Point

--- src/Point.java
+++ src/Point.java
@@ -1,8 +1,8 @@
 
-public class point {
+public class Point {
 	private int x,y,z;
 
-	public point(int x, int y, int z) {
+	public Point(int x, int y, int z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
