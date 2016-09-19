4a15183a13aab53aa368cc185e2dde4d09e2a29f
Mallaury Alaer <mallaury.alaer@etudiant.univ-lille1.fr>
2016-09-19 14:52:27 +0200

    Verification format fichier + option

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,28 @@ public class Main {
 		
 	}
 	
-	public static void main (String args[]){
-		String ext;
-		ext=args[0].substring(args[0].lastIndexOf("."));
+	public static void verificationFormat(String fichier){
+		String ext=fichier.substring(fichier.lastIndexOf("."));
 		
 		if(!ext.equals(".txt")){
 			System.out.println("Format fichier incorrect.");
 		}else{
-			System.out.println("Ok");
+			System.out.println("Format Ok");
 		}
+	}
+	
+	public static void verificationOption(String option){
+		if(option.equals("-f")){
+			System.out.println("Affichage des faces uniquement.");
+		}else if(option.equals("-s")){
+			System.out.println("Affichage des segments uniquement.");
+		}
+	}
+	
+	public static void main (String args[]){
+		verificationFormat(args[0]);
+		verificationOption(args[1]);
+		
 		
 	}
 }
