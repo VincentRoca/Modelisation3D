8d3743e8daa29be6431ee05680fe6758270b4d23
Vincent Roca <vincentroca9@gmail.com>
2016-09-19 15:28:36 +0200

    LectureModele

--- src/LectureModele.java
+++ src/LectureModele.java
@@ -5,9 +5,10 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class LectureModele {
+	
+	private Point[] points;
+	private Face[] faces;
 
-	private int nbVertex;
-	private int nbFace;
 	public LectureModele(){
 		String ligne="";
 		File inputFile ;
@@ -21,15 +22,20 @@ public class LectureModele {
 				while(!(ligne= myBufferedReader.readLine()).equals("end_header")){
 					String[] parts = ligne.split(" ");
 					if(parts[0].equals("element") && parts[1].equals("vertex")){
-						nbVertex=Integer.parseInt(parts[2]);
-						System.out.println(nbVertex);
+						points=new Point[Integer.parseInt(parts[2])];
 					}
 					
 					if(parts[0].equals("element") && parts[1].equals("face")){
-						nbFace=Integer.parseInt(parts[2]);
-						System.out.println(nbFace);
+						faces=new Face[Integer.parseInt(parts[2])];
 					}
 				}
+				
+				for(int i=0; i<points.length; i++) {
+					ligne=myBufferedReader.readLine();
+					String[] parts=ligne.split(" ");
+					points[i]=new Point(Float.parseFloat(parts[0]),Float.parseFloat(parts[1]),Float.parseFloat(parts[2]));
+				}
+				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -39,12 +45,6 @@ public class LectureModele {
 	}
 	
 	
-	public int getNbVertex() {
-		return nbVertex;
-	}
-	public int getNbFace() {
-		return nbFace;
-	}
 	
 	
 }
