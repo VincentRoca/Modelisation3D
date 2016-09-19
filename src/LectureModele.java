38dc880edfea5d25a1bd2932be0a975d03173d24
me <me@172.19.152.111>
2016-09-19 15:15:30 +0200

    LectureModele.java ajout d'attribu avec leurs getters stockant les nombre de point et face dans le HEADER du fichier

--- src/LectureModele.java
+++ src/LectureModele.java
@@ -6,7 +6,8 @@ import java.io.IOException;
 
 public class LectureModele {
 
-	
+	private int nbVertex;
+	private int nbFace;
 	public LectureModele(){
 		String ligne="";
 		File inputFile ;
@@ -17,19 +18,33 @@ public class LectureModele {
 			myReader= new FileReader(inputFile);
 			myBufferedReader = new BufferedReader(myReader);
 			try {
-				while((ligne= myBufferedReader.readLine()) !=null){
-					System.out.println(ligne);
+				while(!(ligne= myBufferedReader.readLine()).equals("end_header")){
+					String[] parts = ligne.split(" ");
+					if(parts[0].equals("element") && parts[1].equals("vertex")){
+						nbVertex=Integer.parseInt(parts[2]);
+						System.out.println(nbVertex);
+					}
+					
+					if(parts[0].equals("element") && parts[1].equals("face")){
+						nbFace=Integer.parseInt(parts[2]);
+						System.out.println(nbFace);
+					}
 				}
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		// à completer
-		
 	}
+	
+	
+	public int getNbVertex() {
+		return nbVertex;
+	}
+	public int getNbFace() {
+		return nbFace;
+	}
+	
+	
 }
