828380a944622bff07ea7b2ccae5d360918af255
Vincent Roca <vincentroca9@gmail.com>
2016-10-17 11:58:58 +0200

    modif ModeleTest

--- test/ModeleTest.java
+++ test/ModeleTest.java
@@ -9,28 +9,28 @@ import java.io.Writer;
 import org.junit.Test;
 
 public class ModeleTest {
-	
+
 	@Test
 	public void testPointEquals() {
-		Point p1=new Point((float)2.5,(float)3,(float)4.2),
-				p2=new Point((float)2.5,(float)3,(float)4.2);
-		assertEquals(p1,p2);
-		p2=new Point((float)2,(float)2.3,(float)4.2);
+		Point p1 = new Point((float) 2.5, (float) 3, (float) 4.2), p2 = new Point((float) 2.5, (float) 3, (float) 4.2);
+		assertEquals(p1, p2);
+		p2 = new Point((float) 2, (float) 2.3, (float) 4.2);
 		assertNotEquals(p1, p2);
 	}
-	
+
 	@Test
 	public void testFaceEquals() {
-		Point[] p1=new Point[]{new Point((float)2,(float)3.2,(float)4),new Point((float)101.5,(float)102.2,(float)1),
-				new Point((float)10,(float)200,(float)10.35)},
-				p2=new Point[]{new Point((float)101.5,(float)102.2,(float)1),new Point((float)2,(float)3.2,(float)4),
-						new Point((float)10,(float)200,(float)10.35)};
-		Face f1=new Face(p1), f2=new Face(p2);
-		assertEquals(f1,f2);
-		f1.getPoints()[2]=new Point((float)23.2,(float)31,(float)34.32);
-		assertNotEquals(f1,f2);
+		Point[] p1 = new Point[] { new Point((float) 2, (float) 3.2, (float) 4),
+				new Point((float) 101.5, (float) 102.2, (float) 1), new Point((float) 10, (float) 200, (float) 10.35) },
+				p2 = new Point[] { new Point((float) 101.5, (float) 102.2, (float) 1),
+						new Point((float) 2, (float) 3.2, (float) 4),
+						new Point((float) 10, (float) 200, (float) 10.35) };
+		Face f1 = new Face(p1), f2 = new Face(p2);
+		assertEquals(f1, f2);
+		f1.getPoints()[2] = new Point((float) 23.2, (float) 31, (float) 34.32);
+		assertNotEquals(f1, f2);
 	}
-	
+
 	@Test
 	public void testLectureModele() {
 		String[] contenuFile=new String[]{
@@ -50,17 +50,27 @@ public class ModeleTest {
 				"3 0 1 3",
 				"3 1 3 2",
 				"3 0 2 3"};
-		File f=new File("fileTestLectureModele");
+		File fichier=new File("fileTestLectureModele");
 		try {
-			Writer writer = new PrintWriter(f);
+			Writer writer = new PrintWriter(fichier);
 			for(String s : contenuFile) 
 				writer.write(s+"\n");
 			writer.close();
-			f.createNewFile();
+			fichier.createNewFile();
 			Modele m=new Modele("fileTestLectureModele");
+			fichier.delete();
 			Face[] faces=m.getFaces();
+			// on crée les points avec décalage de 50 pour x et y
+			Point[] p=new Point[]{new Point((float)52.0,(float)53,(float)4.5),new Point((float)54.3,(float)53.4,(float)100),
+					new Point((float)104,(float)52.95,(float)3.4),new Point((float)128.2,(float)93,(float)1.2)};
+			Face f=new Face(new Point[]{p[0],p[1],p[3]});
+			assertEquals(faces[0],f);
+			f=new Face(new Point[]{p[1],p[3],p[2]});
+			assertEquals(faces[1],f);
+			f=new Face(new Point[]{p[0],p[2],p[3]});
+			assertEquals(faces[2],f);
 		} catch (IOException e) {
-			e.printStackTrace();
+			e.printStackTrace(); fichier.delete();
 		}
 	}
 }
