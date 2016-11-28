654e5a3eeb6a40ba1af166567407f12d6da63dd0
Vanrenta <Vanrenta@172.19.187.46>
2016-11-28 14:18:10 +0100

    Ajout des méthodes update et delete dans Database + Javadoc

--- src/DataBase.java
+++ src/DataBase.java
@@ -60,7 +60,7 @@ public class DataBase {
 				if(filelist[i].endsWith(".ply")){
 					String id=filelist[i].substring(0, filelist[i].length()-4);
 					System.out.println(id);
-					
+
 					//recupere la date
 					Calendar c = Calendar.getInstance();
 					java.util.Date date =  c.getTime();
@@ -68,7 +68,7 @@ public class DataBase {
 					int month =date.getMonth();
 					int day = date.getDay();
 					Date date2 = new Date(year , month, day);
-					
+
 					//requete
 					String s ="insert into modele values('"+id+"','data/"+filelist[i]+"','"+date2+"','"+id+"')";
 
@@ -82,9 +82,67 @@ public class DataBase {
 		}
 		close();
 	}
-	
-	
-	
+
+
+	/**
+	 * Fonction qui permet de mettre à jour un tuple dans la base de données.
+	 * @param idSrc l'identifiant du tuple à modifier.
+	 * @param valeur les mots clés qui décrivent le modele.
+	 * @param path le chemin du modele contenu dans la base de données.
+	 */
+	public void update(String idSrc, String valeur, String path){
+		open();
+
+		try {
+
+			Statement stmt = con.createStatement();
+
+			stmt.execute("select * from modele");
+
+			//recupere la date
+			Calendar c = Calendar.getInstance();
+			java.util.Date date =  c.getTime();
+			int year = date.getYear();
+			int month =date.getMonth();
+			int day = date.getDay();
+			Date date2 = new Date(year , month, day);
+
+			//requete
+
+			String s ="update modele set path='"+path+"', date='"+date2+ "' MotsCles='" + valeur + "', where id='"+idSrc+"'";
+
+			stmt.executeUpdate(s);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		close();
+	}
+
+	/**
+	 * Fonction qui permet de supprimer un tuple dans la base de données.
+	 * @param idSrc l'identifiant du tuple à supprimer.
+	 */
+	public void delete(String idSrc){
+		open();
+
+		try {
+
+			Statement stmt = con.createStatement();
+			stmt.execute("select * from modele");
+
+			//requete
+			String s ="delete from modele where id='"+idSrc+"'";
+			stmt.executeUpdate(s);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		close();
+	}
+
 	public static void main(String[] args){
 		new DataBase().initDB();
 	}
