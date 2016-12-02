0e3c4e7a4f34b161aea4d69406ce1f3949d98def
adrien Lecomte <adrien.lecomte@etudiant.univ-lille1.fr>
2016-12-02 18:09:28 +0100

    tests

---
+++ tests/MatriceFloatTest.java
@@ -0,0 +1,42 @@
+
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MatriceFloatTest {
+
+	@Test
+	public void testProduit() {
+		float [][]f1=new float[2][2];
+		f1[0][0]=2;
+		f1[0][1]=2;
+		f1[1][0]=3;
+		f1[1][1]=1;
+
+		float [][]f2=new float[2][2];
+		f2[0][0]=1;
+		f2[0][1]=3;
+		f2[1][0]=2;
+		f2[1][1]=4;
+		
+		MatriceFloat m1=new MatriceFloat (f1);
+		MatriceFloat m2=new MatriceFloat (f2);
+		
+		MatriceFloat m3=m1.produit(m2);
+
+	//	System.out.println(m3.get(0,0));
+		assertEquals(m3.get(0,0),6, 0.001);
+		assertEquals(m3.get(0,1),14, 0.001);
+		assertEquals(m3.get(1,0),5, 0.001);
+		assertEquals(m3.get(1,1),13, 0.001);
+		
+	}
+
+	@Test
+	public void testTransformation() {
+		fail("Not yet implemented");
+	}
+
+}
