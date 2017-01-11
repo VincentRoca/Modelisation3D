84a49825a2c286dae3bd473ce00ed6bf65d51706
adrien <adrien@AsusROG-Adrien>
2017-01-11 20:41:20 +0100

    Géometrie Test

--- tests/GeometrieTest.java
+++ tests/GeometrieTest.java
@@ -1,66 +1,74 @@
+import static org.junit.Assert.*;
+
+import java.awt.Point;
+
 import org.junit.*;
 
 import maths.Geometrie;
 import maths.MatriceFloat;
+import references.References;
 
 public class GeometrieTest {
 	MatriceFloat m1;
-	@Before
+	//@Before
 	public void genereMatrice(){
 		float [][]f1=new float[4][4];
-		f1[0][0]=2; f1[0][1]=2; f1[0][2]=2; f1[0][3]=2;
-		f1[1][0]=2; f1[1][1]=2; f1[1][2]=2; f1[1][3]=2;
-		f1[2][0]=4; f1[2][1]=2; f1[2][2]=2; f1[2][3]=2;
-		f1[3][0]=12; f1[3][1]=2; f1[3][2]=2; f1[3][3]=2;
+		f1[0][0]=2; f1[0][1]=2; f1[0][2]=2; f1[0][3]=1;
+		f1[1][0]=2; f1[1][1]=2; f1[1][2]=2; f1[1][3]=1;
+		f1[2][0]=2; f1[2][1]=2; f1[2][2]=2; f1[2][3]=1;
+		f1[3][0]=2; f1[3][1]=2; f1[3][2]=2; f1[3][3]=1;
 		
 		m1 = new MatriceFloat(f1);
 	}
 
 	@Test
 	public void testTranslation() {
-		//genereMatrice();
+		genereMatrice();
 		m1.transformation(Geometrie.translation(1, 1, 1));
-		System.out.println(m1);
-		/*for (int i = 1; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				testTranslation2(i, j);	
-			}
-		}*/
-	}
+		
+		assertEquals(m1.get(0,0),3,0.1);
+		assertEquals(m1.get(0,1),3,0.1);
+		assertEquals(m1.get(0,2),3,0.1);
+		assertEquals(m1.get(0,3),1,0.1);
+
+		assertEquals(m1.get(1,0),3,0.1);
+		assertEquals(m1.get(1,1),3,0.1);
+		assertEquals(m1.get(1,2),3,0.1);
+		assertEquals(m1.get(1,3),1,0.1);
+
+		assertEquals(m1.get(2,0),3,0.1);
+		assertEquals(m1.get(2,1),3,0.1);
+		assertEquals(m1.get(2,2),3,0.1);
+		assertEquals(m1.get(2,3),1,0.1);
+
+		assertEquals(m1.get(3,0),3,0.1);
+		assertEquals(m1.get(3,1),3,0.1);
+		assertEquals(m1.get(3,2),3,0.1);
+		assertEquals(m1.get(3,3),1,0.1);
 	
-	public void testTranslation2(int i,int j) {
-		System.out.println(i+" , "+j+" : "+m1.get(i, j)+"\n");
-		//assertEquals(m1.get(i, j),i+1,0.1);
 	}
-	
 	@Test
 	public void testHomothetie() {
-		//fail("Not yet implemented");
-	}
+		genereMatrice();
+		m1.transformation(Geometrie.homothetie((float) 1.5,1,1,1));
+		assertEquals(m1.get(0,0),2.5,0.1);
+		assertEquals(m1.get(0,1),2.5,0.1);
+		assertEquals(m1.get(0,2),2.5,0.1);
+		assertEquals(m1.get(0,3),1,0.1);
 
-	@Test
-	public void testCadrage() {
-		//fail("Not yet implemented");
-	}
+		assertEquals(m1.get(1,0),2.5,0.1);
+		assertEquals(m1.get(1,1),2.5,0.1);
+		assertEquals(m1.get(1,2),2.5,0.1);
+		assertEquals(m1.get(1,3),1,0.1);
 
-	@Test
-	public void testRotationX() {
-		//fail("Not yet implemented");
-	}
+		assertEquals(m1.get(2,0),2.5,0.1);
+		assertEquals(m1.get(2,1),2.5,0.1);
+		assertEquals(m1.get(2,2),2.5,0.1);
+		assertEquals(m1.get(2,3),1,0.1);
 
-	@Test
-	public void testRotationZ() {
-		//fail("Not yet implemented");
+		assertEquals(m1.get(3,0),2.5,0.1);
+		assertEquals(m1.get(3,1),2.5,0.1);
+		assertEquals(m1.get(3,2),2.5,0.1);
+		assertEquals(m1.get(3,3),1,0.1);
 	}
-
-	@Test
-	public void testRotationY() {
-		//fail("Not yet implemented");
-	}
-
-	@Test
-	public void testIsobarycentre() {
-		//fail("Not yet implemented");
-	}
-
 }
